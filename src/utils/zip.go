da04c96c3142b7e1d9bb661bf42fdbaf493cc2ba
NhanPT <github@help14.com>
2023-01-07 12:45:24 +0700

    debug compress folder

--- src/utils/zip.go
+++ src/utils/zip.go
@@ -2,53 +2,130 @@ package utils
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func ZipFolder(inputPath string, outputPath string) error {
-	file, err := os.Create(outputPath)
+	return CompressZip(inputPath, outputPath)
+
+	// file, err := os.Create(outputPath)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer file.Close()
+
+	// w := zip.NewWriter(file)
+	// defer w.Close()
+
+	// walker := func(path string, info os.FileInfo, err error) error {
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	if info.IsDir() {
+	// 		return nil
+	// 	}
+	// 	file, err := os.Open(path)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	defer file.Close()
+
+	// 	// Ensure that `path` is not absolute; it should not start with "/".
+	// 	// This snippet happens to work because I don't use
+	// 	// absolute paths, but ensure your real-world code
+	// 	// transforms path into a zip-root relative path.
+	// 	f, err := w.Create(path)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// 	_, err = io.Copy(f, file)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// 	return nil
+	// }
+	// err = filepath.Walk(inputPath, walker)
+	// if err != nil {
+	// 	return err
+	// }
+	// return nil
+}
+
+func CompressZip(inputPath string, outputPath string) error {
+	archive, err := os.Create(outputPath)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer archive.Close()
 
-	w := zip.NewWriter(file)
+	w := zip.NewWriter(archive)
 	defer w.Close()
 
-	walker := func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-
-		// Ensure that `path` is not absolute; it should not start with "/".
-		// This snippet happens to work because I don't use
-		// absolute paths, but ensure your real-world code
-		// transforms path into a zip-root relative path.
-		f, err := w.Create(path)
-		if err != nil {
-			return err
-		}
-
-		_, err = io.Copy(f, file)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	}
-	err = filepath.Walk(inputPath, walker)
+	stat, err := os.Stat(inputPath)
 	if err != nil {
 		return err
 	}
+
+	if stat.IsDir() {
+		dir := inputPath
+		err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			}
+			arPath := strings.ReplaceAll(path, dir, "")
+			arPath = strings.TrimPrefix(arPath, "/")
+			f, err := w.Create(arPath)
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			}
+			in, err := os.Open(path)
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			}
+			_, err = io.Copy(f, in)
+			if err != nil {
+				fmt.Println(err.Error())
+				return err
+			}
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	} else {
+		path := inputPath
+		dir := filepath.Dir(path)
+		arPath := strings.ReplaceAll(path, dir, "")
+		arPath = strings.TrimPrefix(arPath, "/")
+		f, err := w.Create(arPath)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+		in, err := os.Open(path)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+		_, err = io.Copy(f, in)
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+	}
+
 	return nil
 }
